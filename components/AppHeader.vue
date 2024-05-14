747ad36f081bd3d99f7a46b702190eb5de479436
Reza Jafar <rezajafar26@gmail.com>
2024-05-14 11:05:23 -0700

    chore: Add initial project files and configurations

---
+++ components/AppHeader.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import type { NavItem } from "@nuxt/content/dist/runtime/types";
+
+const navigation = inject<Ref<NavItem[]>>("navigation", ref([]));
+
+const links = [
+  {
+    label: "Ride",
+    to: "/docs",
+  },
+  {
+    label: "Drive",
+    to: "/pricing",
+  },
+  {
+    label: "Operate",
+    to: "/blog",
+  },
+];
+</script>
+
+<template>
+  <UHeader :links="links">
+    <template #logo>
+      RideHealthy <UBadge label="Beta" variant="subtle" class="mb-0.5" />
+    </template>
+
+    <template #right>
+      <UButton label="Sign in" color="gray" to="/login" />
+      <UButton
+        label="Sign up"
+        icon="i-heroicons-arrow-right-20-solid"
+        trailing
+        color="black"
+        to="/signup"
+        class="hidden lg:flex"
+      />
+    </template>
+
+    <template #panel>
+      <UNavigationTree :links="mapContentNavigation(navigation)" default-open />
+    </template>
+  </UHeader>
+</template>
